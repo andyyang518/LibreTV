60417e43faeecf7335a4e0a46177099bbdf759cc
andyyang518 <155098906+andyyang518@users.noreply.github.com>
2025-07-06 17:44:42 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -106,7 +106,7 @@ const API_SITES = {
     // 而作为观众无辜吗？毫无疑问，他们促成了黄色产业链的再生产。
     // 我们提供此警告，是希望您能认清这些内容的本质——它们是压迫和奴役的工具，而非娱乐。
     slapibf: {
-        api: 'https://slapibf.com/api.php/provide/vod/?ac=list',
+        api: 'https://slapibf.com/api.php/provide/vod/at/xml',
         name: '森林资源',
         adult: true
     },
